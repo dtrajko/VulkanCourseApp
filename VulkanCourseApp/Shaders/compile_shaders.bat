04286eeb82a17058e344c0bc2acdfdf74abebdf5
Dejan Trajkovic <dtrajko@gmail.com>
2020-04-05 18:54:04 +0200

    12. Graphics Pipeline Part 1: Shader Modules (GLSL shader scripts and SPIR-V compiler)

---
+++ VulkanCourseApp/Shaders/compile_shaders.bat
@@ -0,0 +1,3 @@
+D:/VulkanSDK/1.1.130.0/Bin32/glslangValidator.exe -V shader.vert
+D:/VulkanSDK/1.1.130.0/Bin32/glslangValidator.exe -V shader.frag
+pause
---
+++ compile_overv.bat
@@ -0,0 +1,4 @@
+D:/VulkanSDK/1.1.130.0/Bin32/glslc.exe -fshader-stage=vertex   ./VulkanCourseApp/Shaders/shader.vert -o ./VulkanCourseApp/Shaders/shader_vertex.spv
+D:/VulkanSDK/1.1.130.0/Bin32/glslc.exe -fshader-stage=fragment ./VulkanCourseApp/Shaders/shader.frag -o ./VulkanCourseApp/Shaders/shader_fragment.spv
+
+pause
