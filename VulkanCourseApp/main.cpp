3a5dec0fd54df79a6f1de3db22f7b6c21e742fe8
Dejan Trajkovic <dtrajko@gmail.com>
2020-04-14 02:54:25 +0200

    Old House 2 model

--- VulkanCourseApp/main.cpp
+++ VulkanCourseApp/main.cpp
@@ -41,7 +41,7 @@ int main()
 	float deltaTime = 0.0f;
 	float lastTime = 0.0f;
 
-	int modelID = vulkanRenderer.createMeshModel("Models/Seahawk.obj");
+	int modelID = vulkanRenderer.createMeshModel("Models/OldHouse2.obj");
 
 	// Loop until closed
 	while (!glfwWindowShouldClose(window))
@@ -58,7 +58,7 @@ int main()
 		glm::mat4 modelMatrix(1.0f);
 		modelMatrix = glm::translate(modelMatrix, glm::vec3(0.0f, 0.0f, 0.0f));
 		modelMatrix = glm::rotate(modelMatrix, glm::radians(angle), glm::vec3(0.0f, 1.0f, 0.0f));
-		modelMatrix = glm::scale(modelMatrix, glm::vec3(0.2f));
+		modelMatrix = glm::scale(modelMatrix, glm::vec3(0.1f));
 
 		vulkanRenderer.updateModel(modelID, modelMatrix);
 
